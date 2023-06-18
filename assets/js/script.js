86d17d30c720bbe202892f572f0d419a7bbd1058
josealexanderdrt <josealexanderdrt@gmail.com>
2023-06-17 20:59:48 -0400

    cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,1 @@
 
-// var nombre = prompt("Ingrese su nombre");
-//alert("Hola" )
-
-//var num1 = prompt("ingrese un número");
-//var num2 = prompt("ingrese otro número");
-//resultado = Number (num1) + Number (num2)
-//alert("El Resultado es: " + resultado);
-
-var texto = document.querySelector("p")
-var direccion = prompt(" ingrese direccion")
-var dpto = prompt(" ingrese la dpto")
-var comuna = prompt(" ingrese comuna")
-texto.innerHTML = "El arrendador declara ser el dueño maximo, que queda en " +  direccion   +  dpto  +  comuna 
