143f817bea589f6ec55b99a36a216c878539b52c
josealexanderdrt <josealexanderdrt@gmail.com>
2023-06-18 11:06:32 -0400

    Resultado final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
 
+let precio = 875000
+let cantidad = document.querySelector("#inCantidad")
+let colorSeleccionado = document.querySelector("#inColor")
+let button = document.querySelector("#calcularTotal")
+
+
+
+button.addEventListener("click", () =>{
+    document.querySelector("#resumenTotal").innerHTML= cantidad.value * precio;
+    document.querySelector("#resumenCantidad").innerHTML= cantidad.value;
+    document.querySelector("#resumenColor").style.backgroundColor= colorSeleccionado.value;
+
+
+})
+
+
+let limpiar = document.querySelector("#limpiar")
+
+limpiar.addEventListener("click", () =>{document.querySelector("#resumenTotal").innerHTML= 0;
+document.querySelector("#resumenCantidad").innerHTML= "0";
+document.querySelector("#resumenColor").innerHTML= "";
+document.querySelector("#inColor").value= "";
+document.querySelector("#inCantidad").value= "";
+
+    
+
+
+})
+
+
+
